e923fd5823a3adfc86a38f800c178aa631e358f6
rathinreddy1402 <shobharani0599@gmail.com>
2023-01-21 11:50:26 +0530

    2nd commit

--- sample/src/test/java/sample/Test.java
+++ sample/src/test/java/sample/Test.java
@@ -5,8 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
+		System.out.println("hello");
 		
 		//done madam its working
+	
 	}
 
 }
